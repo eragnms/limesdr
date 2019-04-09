ce99741f1874a26381d3e1d8473f24cd0baa7b03
Mats Gustafsson <mats@mcnab.se>
2019-04-09 07:05:34 +0200

    modifies the guard samples for ping, does not work with a small guard window

--- include/sdr_config.h
+++ include/sdr_config.h
@@ -74,7 +74,7 @@ struct SDR_Device_Config
         int64_t max_sync_error = 5;
         uint32_t threshold_factor = 32;
         size_t num_of_ping_tries = 1000; //!< Number of tries before initial sync
-        int64_t ping_burst_guard = 100000; //!< Guard samples around expected PING
+        int64_t ping_burst_guard = 1000000; //!< Guard samples around expected PING
 
         bool tx_active = true;
         bool rx_active = true;
