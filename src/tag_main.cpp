cb1726a818b33f0faf28faa31b1552fd458ff539
Mats Gustafsson <mats@mcnab.se>
2019-04-12 17:11:25 +0200

    mods to beacon

--- src/tag_main.cpp
+++ src/tag_main.cpp
@@ -67,8 +67,8 @@ void list_device_info()
 void run_tag(bool plot_data)
 {
         SDR_Device_Config dev_cfg;
-        //std::string dev_serial = dev_cfg.serial_bladerf_x40;
-        std::string dev_serial = dev_cfg.serial_bladerf_xA4;
+        std::string dev_serial = dev_cfg.serial_bladerf_x40;
+        //std::string dev_serial = dev_cfg.serial_bladerf_xA4;
         //std::string dev_serial = dev_cfg.serial_lime_3;
         //dev_cfg.tx_active = false;
         dev_cfg.is_beacon = false;
